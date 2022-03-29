282414c1542d274cc6eac9c4296a8f2223314fda
Slava <81441961+Hil-sys@users.noreply.github.com>
2022-03-29 19:23:33 +0300

    Add files via upload

---
+++ tr-d.step.py
@@ -0,0 +1,10 @@
+# a = 5
+# while a == 5:
+#     try:
+#         a = int(input('Vvod chisla: '))
+#         a += 5
+#         print(a)
+#     except ValueError:
+#         print('Vii vveli ne chislo!')
+#     finally:
+#         print('Yra!')
